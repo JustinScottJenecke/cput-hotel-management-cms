33e7c713372b56c13276864ce09f4003c94a50ce
Justin <justinjenecke@gmail.com>
2021-08-24 16:01:11 +0200

    completes repository implementations for cleaningstaff and frontdeskstaff

--- src/main/java/za/ac/cput/repository/staff/impl/CleaningStaffRepository.java
+++ src/main/java/za/ac/cput/repository/staff/impl/CleaningStaffRepository.java
@@ -1,4 +1,66 @@
 package za.ac.cput.repository.staff.impl;
 
-public class CleaningStaffRepository {
+import za.ac.cput.entity.staff.CleaningStaff;
+import za.ac.cput.entity.staff.KitchenStaff;
+import za.ac.cput.repository.staff.ICleaningStaffRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CleaningStaffRepository implements ICleaningStaffRepository {
+
+    private static CleaningStaffRepository cleaningStaffRepository = null;
+    private List<CleaningStaff> cleaningStaffDB = null;
+
+    private CleaningStaffRepository() {
+        cleaningStaffDB = new ArrayList<CleaningStaff>();
+    }
+
+    public static CleaningStaffRepository getCleaningStaffRepository() {
+
+        if (cleaningStaffRepository == null)
+            cleaningStaffRepository = new CleaningStaffRepository();
+
+        return cleaningStaffRepository;
+    }
+
+    @Override
+    public CleaningStaff create(CleaningStaff x) {
+        boolean created = cleaningStaffDB.add(x);
+        if (created)
+            return x;
+        else
+            return null;
+    }
+
+    @Override
+    public CleaningStaff read(String s) {
+        for (CleaningStaff man : cleaningStaffDB) {
+            if (man.getStaffId() == s)
+                return man;
+        }
+        return null;
+    }
+
+    @Override
+    public CleaningStaff update(CleaningStaff x) {
+        CleaningStaff oldManager = read(x.getStaffId());
+        if (oldManager != null) {
+            cleaningStaffDB.remove(oldManager);
+            cleaningStaffDB.add(x);
+            return x;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) {
+
+        CleaningStaff delManager = read(s);
+        if (delManager == null)
+            return false;
+        cleaningStaffDB.remove(delManager);
+        return true;
+
+    }
 }
--- src/main/java/za/ac/cput/repository/staff/impl/FrontDeskStaffRepository.java
+++ src/main/java/za/ac/cput/repository/staff/impl/FrontDeskStaffRepository.java
@@ -1,4 +1,67 @@
 package za.ac.cput.repository.staff.impl;
 
-public class FrontDeskStaffRepository {
+import za.ac.cput.entity.staff.FrontDeskStaff;
+import za.ac.cput.repository.staff.IFrontDeskStaffRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FrontDeskStaffRepository implements IFrontDeskStaffRepository {
+
+    private static FrontDeskStaffRepository frontDeskStaffRepository = null;
+    private List<FrontDeskStaff> frontDeskStaffDB = null;
+
+    private FrontDeskStaffRepository(){
+        frontDeskStaffDB = new ArrayList<FrontDeskStaff>();
+    }
+
+    public static FrontDeskStaffRepository getFontDeskStaffRepository(){
+
+        if (frontDeskStaffRepository == null)
+            frontDeskStaffRepository = new FrontDeskStaffRepository();
+
+        return frontDeskStaffRepository;
+    }
+
+    @Override
+    public FrontDeskStaff create(FrontDeskStaff x) {
+        boolean created = frontDeskStaffDB.add(x);
+        if (created)
+            return x;
+        else
+            return null;
+    }
+
+    @Override
+    public FrontDeskStaff read(String s) {
+        for (FrontDeskStaff man:frontDeskStaffDB) {
+            if (man.getStaffId() == s)
+                return man;
+        }
+        return null;
+    }
+
+    @Override
+    public FrontDeskStaff update(FrontDeskStaff x) {
+        FrontDeskStaff oldManager = read(x.getStaffId());
+        if (oldManager != null){
+            frontDeskStaffDB.remove(oldManager);
+            frontDeskStaffDB.add(x);
+            return x;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean delete(String s) {
+
+        FrontDeskStaff delManager = read(s);
+        if (delManager == null)
+            return false;
+        frontDeskStaffDB.remove(delManager);
+        return true;
+
+
+    }
+
 }
