540c935f0c144b2a962bf8170daee6944469036e
Justin <justinjenecke@gmail.com>
2021-08-25 10:33:55 +0200

    populates Manager service with repo and crud functions

--- src/main/java/za/ac/cput/service/staff/impl/ManagerService.java
+++ src/main/java/za/ac/cput/service/staff/impl/ManagerService.java
@@ -1,26 +1,41 @@
 package za.ac.cput.service.staff.impl;
 
 import za.ac.cput.entity.staff.Manager;
+import za.ac.cput.repository.staff.impl.ManagerRepository;
 import za.ac.cput.service.staff.IMangerService;
 
 public class ManagerService implements IMangerService {
+
+    private static ManagerService service = null;
+    private ManagerRepository repository = null;
+
+    private ManagerService(){
+        this.repository = ManagerRepository.getManagerRepository();
+    }
+
+    public static ManagerService getService() {
+        if ( service== null)
+            service = new ManagerService();
+        return service;
+    }
+
     @Override
     public Manager create(Manager x) {
-        return null;
+        return this.repository.create(x);
     }
 
     @Override
     public Manager read(String ID) {
-        return null;
+        return this.repository.read(ID);
     }
 
     @Override
     public Manager update(Manager x) {
-        return null;
+        return this.repository.update(x);
     }
 
     @Override
     public boolean delete(String ID) {
-        return false;
+        return this.repository.delete(ID);
     }
 }
