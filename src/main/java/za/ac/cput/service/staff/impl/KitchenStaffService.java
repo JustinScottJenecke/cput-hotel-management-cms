74150f5bfed0c323559b4da167ee52c043f8a9ec
Justin <justinjenecke@gmail.com>
2021-08-25 10:30:09 +0200

    populates kitchen staff service with repo and crud functions

--- src/main/java/za/ac/cput/service/staff/impl/KitchenStaffService.java
+++ src/main/java/za/ac/cput/service/staff/impl/KitchenStaffService.java
@@ -1,26 +1,41 @@
 package za.ac.cput.service.staff.impl;
 
 import za.ac.cput.entity.staff.KitchenStaff;
+import za.ac.cput.repository.staff.impl.KitchenStaffRepository;
 import za.ac.cput.service.staff.IKitchenStaffService;
 
 public class KitchenStaffService implements IKitchenStaffService {
+
+    private static KitchenStaffService service = null;
+    private KitchenStaffRepository repository = null;
+
+    private KitchenStaffService(){
+        this.repository = KitchenStaffRepository.getKitchenStaffRepository();
+    }
+
+    public static KitchenStaffService getService() {
+        if ( service== null)
+            service = new KitchenStaffService();
+        return service;
+    }
+
     @Override
     public KitchenStaff create(KitchenStaff x) {
-        return null;
+        return this.repository.create(x);
     }
 
     @Override
     public KitchenStaff read(String ID) {
-        return null;
+        return this.repository.read(ID);
     }
 
     @Override
     public KitchenStaff update(KitchenStaff x) {
-        return null;
+        return this.repository.update(x);
     }
 
     @Override
     public boolean delete(String ID) {
-        return false;
+        return this.repository.delete(ID);
     }
 }
